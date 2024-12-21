47591ac5ef258b1e9338e81896f3b87e3cf40dc5
madhuri801 <chandakamadhuri16@gmail.com>
2024-12-21 10:29:29 +0530

    Add files via upload

---
+++ terraform project on s3/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAQLVQQ5EHRBLSHZN2"
+  secret_key = "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"
+  
+}
---
+++ terraform project on s3/variables.tf
@@ -0,0 +1,4 @@
+#variable.tf
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
